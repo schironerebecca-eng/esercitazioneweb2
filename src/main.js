270848210349d21304b1d888727f7d4cb618e0fd
Rebecca <schironerebecca@gmail.com>
2026-03-31 13:14:22 +0200

    Ho aggiunto index.html

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "./style.scss";
 // import 'bootstrap/js/dist/alert';
 // import 'bootstrap/js/dist/button';
 // import 'bootstrap/js/dist/carousel';
- import 'bootstrap/js/dist/collapse';
+import "bootstrap/js/dist/collapse";
 // import 'bootstrap/js/dist/dropdown';
 // import 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/offcanvas';
@@ -12,3 +12,43 @@ import "./style.scss";
 // import 'bootstrap/js/dist/tab';
 // import 'bootstrap/js/dist/toast';
 // import 'bootstrap/js/dist/tooltip';
+
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#hero h1", {
+  scale: 0.5,
+  duration: 1,
+  opacity: 0,
+  rotate: 15, 
+});
+
+gsap.from("#hero p", {
+  x: 500,
+  duration: 1,
+  opacity: 0,
+});
+
+gsap.from("#hero .btn", {
+  yPercent: 100,
+  duration: 1,
+  opacity: 0,
+  repeat: -1,
+  yoyo: true,
+  backgroundColor: "yellow",
+  // fontweight: 100,
+});
+
+gsap.from("#chi-siamo h2" ,{
+  scale: 0.5,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    start: "center 50%",
+    markers: true,
+    trigger: "#chi-siamo"
+  }
+});
